ac688eda8ded1fab1a54832bb9ec92d5ded1acdf
darwin.apolinario12 <darwin.apolinario12@gmail.com>
2022-04-08 02:12:49 +0800

    initialize zesty-explorer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
-import ZestyExplorer from "./component/ZestyExplorer";
+// import ZestyExplorer from "./component/ZestyExplorer";
 
 function App() {
-  return <ZestyExplorer />;
+  return <div style={{ background: "red", height: "100vh" }}>wlkejrlkwe</div>;
 }
 
 export default App;
