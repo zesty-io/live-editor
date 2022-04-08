7f7f075a2953a8a52056aecf4bafe679b426a903
darwin.apolinario12 <darwin.apolinario12@gmail.com>
2022-04-09 00:54:43 +0800

    change color of codeblock to zestydarkblue

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-export * from "./component/ZestyExplorer.js";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("zesty-explorer"));
