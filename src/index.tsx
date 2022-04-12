6fe134c386d63cd84df37a1fdc3eb4cb28e519b5
darwin808 <darwin.apolinario12@gmail.com>
2022-04-12 17:01:29 +0800

    :art: (dist): dis
    t

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom"
 import { ZestyExplorer } from "./views"
 import { canUseDOM } from "./utils/index"
 
+console.log("darwin")
 const main = () => {
    if (!canUseDOM()) {
       return null
