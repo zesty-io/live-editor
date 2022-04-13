048eb660bae9da8c3415eab9db015a4bdd793136
darwin808 <darwin.apolinario12@gmail.com>
2022-04-14 02:32:08 +0800

    cdn udate

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,16 @@
-export * from "./components/ZestyExplorer"
+/* eslint-disable react/no-render-return-value */
+import React from "react"
+import ReactDOM from "react-dom"
+import { ZestyExplorer } from "./components"
+import { canUseDOM } from "./utils/index"
+
+console.log("latest apr 12")
+const main = () => {
+   if (!canUseDOM()) {
+      return null
+   }
+   document.body.innerHTML += '<div id="zesty-explorer"></div>'
+   return ReactDOM.render(<ZestyExplorer />, document.getElementById("zesty-explorer"))
+}
+
+main()
