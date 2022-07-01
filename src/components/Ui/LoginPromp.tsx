edb439d9c47f05e00f1fba276b58e653a4d4bb6b
darwin808 <darwin.apolinario12@gmail.com>
2022-07-01 20:00:59 +0800

    feat(loginrom.ts): added logo

--- src/components/Ui/LoginPromp.tsx
+++ src/components/Ui/LoginPromp.tsx
@@ -1,11 +1,14 @@
 import { Box, Typography } from "@mui/material"
 import { NormalBtn } from "components"
+import { assets } from "constants"
 import React from "react"
 
 const loginPromp = {
    display: "flex",
    flexDirection: "column",
    justifyItems: "center",
+   justifyContent: "center",
+   alignItems: "center",
    position: "fixed",
    top: "25%",
    left: "15%",
@@ -17,6 +20,31 @@ const loginPromp = {
 export const LoginPrompt = () => {
    return (
       <Box sx={loginPromp}>
+         <Box
+            paddingTop={1}
+            sx={{
+               width: "100%",
+               display: "flex",
+               flexDirection: "row",
+               justifyItems: "center",
+               justifyContent: "center",
+               alignItems: "center",
+               gap: ".5rem",
+            }}
+         >
+            <img
+               src={assets.zestyLogo}
+               alt="Zesty Explorer"
+               width={"100"}
+               height={"100"}
+            />
+            <img
+               src={assets.zestyName}
+               alt="Zesty Explorer"
+               width={"150"}
+               height={"150"}
+            />
+         </Box>
          <Typography sx={{ fontSize: "32px", fontWeight: "bold" }}>
             Please Login
          </Typography>
