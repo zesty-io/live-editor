b76c870bfcfddc6405e26555740ffd8867b6566e
darwin808 <darwin.apolinario12@gmail.com>
2022-07-07 17:24:55 +0800

    feat(codehelper.ts): added type

--- src/views/CodeHelperTab/CodeHelperTab.tsx
+++ src/views/CodeHelperTab/CodeHelperTab.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { CodeHelperTable, GotoTopBtn } from "components"
 import { Box } from "@mui/material"
+import * as helper from "utils"
 interface Props {
    data: any
    metaData: any
@@ -24,8 +25,29 @@ export const CodeHelperTab = ({
    token,
    response,
 }: Props) => {
+   const [fields, setfields] = React.useState([])
    const newData =
       Object.keys(data?.content).length === 0 ? metaData?.data?.data : data?.content
+
+   const userAppSID = helper.getUserAppSID()
+   const modelZUID = newData?.meta?.model?.zuid
+   const instanceZUID = helper.headerZUID(response)
+
+   // @ts-ignore
+   const ZestyAPI = new Zesty.FetchWrapper(
+      instanceZUID,
+      userAppSID,
+      helper.fetchWrapperOptions(),
+   )
+   const getFields = async () => {
+      const res = await ZestyAPI.getFields(modelZUID)
+      res?.data?.length !== 0 && setfields(res?.data)
+   }
+
+   React.useEffect(() => {
+      fields?.length === 0 && getFields()
+   }, [fields])
+
    return (
       <Box
          sx={{
@@ -38,6 +60,7 @@ export const CodeHelperTab = ({
          <GotoTopBtn scrollPos={scrollPos} />
 
          <CodeHelperTable
+            fields={fields}
             response={response}
             content={content}
             onScroll={scrollEvent}
