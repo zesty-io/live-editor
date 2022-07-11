9b53373533bb6bc9a75a781f78f05f4b76ae9ae2
darwin808 <darwin.apolinario12@gmail.com>
2022-07-11 18:40:08 +0800

    chore(index.ts): remove clgs

--- src/index.tsx
+++ src/index.tsx
@@ -24,5 +24,5 @@ export const ZestyLiveEditor = (content?: any) => {
    root.render(<ZestyExplorer content={content} />)
 }
 
-// ZestyLiveEditor()
+ZestyLiveEditor()
 // ZestyLiveEditor(dummydata)
