b54743130fe2e277007763acdbcbcfaf8fc05054
darwin808 <darwin.apolinario12@gmail.com>
2022-07-13 13:59:17 +0800

    test(cypress): added additional config and tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,9 +3,8 @@ import { defineConfig } from "cypress"
 
 export default defineConfig({
    videoCompression: false,
+   video: false,
    env: {
-      email: "darwin.apolinario@zesty.io",
-      password: "Gand27ef!",
       API_AUTH: "https://auth.api.dev.zesty.io",
       COOKIE_NAME: "DEV_APP_SID",
    },
