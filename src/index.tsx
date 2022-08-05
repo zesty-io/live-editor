4c01df35eb1384e76ab835693064c6f1bada5ed1
darwin808 <darwin.apolinario12@gmail.com>
2022-08-05 23:13:26 +0800

    chore(index): remove clgs

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from "react"
 import { ZestyExplorer } from "./components"
+import { createRoot } from "react-dom/client"
 import { createZestyDiv } from "utils"
 import * as helper from "utils/index"
 import { dummydata } from "constants"
-import { render } from "react-dom"
 
 if (process.env.NODE_ENV === "production") {
    console.log = () => {}
-   // console.error = () => {}
-   // console.debug = () => {}
+   console.error = () => {}
+   console.debug = () => {}
 }
 
 console.log(" ********** Zesty live editor loaded v1 ************ ")
@@ -20,7 +20,8 @@ export const ZestyLiveEditor = (content?: any) => {
    createZestyDiv()
 
    const container = document.getElementById("zesty-explorer")
-   render(<ZestyExplorer content={content} />, container)
+   const root = createRoot(container!)
+   root.render(<ZestyExplorer content={content} />)
 }
 
 process.env.NODE_ENV === "development" ? ZestyLiveEditor(dummydata) : ZestyLiveEditor()
